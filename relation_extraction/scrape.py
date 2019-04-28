70195af9033603ad1965ea44d5dcbce3dac94d8a
Ziwei Gu <zg48@cornell.edu>
2019-04-28 17:03:40 -0400

    integrate things

--- relation_extraction/scrape.py
+++ relation_extraction/scrape.py
@@ -1,20 +1,20 @@
 from bs4 import BeautifulSoup
 import requests
+import sys
 
 def read_page(url):
     url_title = url[url.rfind('wiki')+5:]
-    myFile = open('./' + url_title + '_raw_data.txt', 'w', encoding="utf-8")
+    myFile = open('data/raw_data.txt', 'w', encoding="utf-8")
     print("reading page: " + url_title)
     page = requests.get(url)
     try:
         soup = BeautifulSoup(page.text, "html5lib")
-        text = soup.find_all('p')
+        text = soup.find("div",{"class": "mw-parser-output"})
         full_text = ""
-        title = soup.find('h1').getText() + "\n"
-        for t in text:
+        toc = text.find("div", {"class": "toc"})
+        for t in toc.previous_siblings:
              if not t.find('img') and (t.name == 'p' or t.name == 'ul'):
-                full_text += str(t.getText().replace('\n', ''))
-        #myFile.write(title)
+                full_text += str(t.getText().replace('\n', ' '))
         myFile.write(full_text + "\n")
     except AttributeError:
         print("invalid page, skipping")
