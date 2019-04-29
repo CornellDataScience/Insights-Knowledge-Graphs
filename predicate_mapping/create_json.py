ab98131790ddff85159f0ed7e4f48f76f181a8c2
Ziwei Gu <zg48@cornell.edu>
2019-04-28 22:39:00 -0400

    Add color to nodes

--- predicate_mapping/create_json.py
+++ predicate_mapping/create_json.py
@@ -1,5 +1,7 @@
 import json
 import numpy as np
+import en_core_web_sm
+
 
 def get_ids(idfile):
     with open(idfile, 'r') as infile:
@@ -14,9 +16,11 @@ def to_json(eidfile, ridfile, triplefile, combined_rels, jsonfile):
     jsondict = {"nodes": [], "links": []}
     entities = get_ids(eidfile)
     relations = get_ids(ridfile)
-    
+
+    nlp = en_core_web_sm.load()
+    f = lambda x: '#69b3a2' if [token for token in nlp(x) if token.ent_type_ == 'PERSON'] == [] else '#589bdd'
     for i in range(len(entities)):
-        jsondict["nodes"].append({"id": i, "name": entities[i].replace('_', ' ')})
+        jsondict["nodes"].append({"id": i, "name": entities[i].replace('_', ' '), "color": f(entities[i].replace('_', ' '))})
         
     with open(triplefile, 'r') as infile:
         for line in infile.read().split('\n'):
